0edff1080a7718bacb2d5a00af4edad5fdca2b7e
Salome Rodriguez Moscoso <160194456+Salome98342@users.noreply.github.com>
2026-06-08 13:53:43 -0500

    Update RoleHomeLayout.tsx

--- frontend/src/components/RoleHomeLayout.tsx
+++ frontend/src/components/RoleHomeLayout.tsx
@@ -60,7 +60,7 @@ const RoleHomeLayout: React.FC<RoleHomeLayoutProps> = ({
           title: 'Manual',
           imageSrc: '/carrusel/Manual.png',
           alt: 'Manual de usuario',
-          href: 'https://drive.google.com/file/d/1NIDqQM2nYDMD685JQjRrkfjM9gMxl5P6/view',
+          href: 'https://drive.google.com/file/d/1NIDqQM2nYDMD685JQjRrkfjM9gMxl5P6/view?usp=sharing',
           hrefLabel: 'Abrir manual',
         },
       ]
@@ -75,7 +75,7 @@ const RoleHomeLayout: React.FC<RoleHomeLayoutProps> = ({
           title: 'Manual',
           imageSrc: '/carrusel/Manual.png',
           alt: 'Manual de usuario',
-          href: 'https://drive.google.com/file/d/1NIDqQM2nYDMD685JQjRrkfjM9gMxl5P6/view',
+          href: 'https://drive.google.com/file/d/1NIDqQM2nYDMD685JQjRrkfjM9gMxl5P6/view?usp=sharing',
           hrefLabel: 'Abrir manual',
         },
       ]
@@ -89,7 +89,7 @@ const RoleHomeLayout: React.FC<RoleHomeLayoutProps> = ({
         title: 'Manual',
         imageSrc: '/carrusel/Manual.png',
         alt: 'Manual de usuario',
-        href: 'https://drive.google.com/file/d/1NIDqQM2nYDMD685JQjRrkfjM9gMxl5P6/view',
+        href: 'https://drive.google.com/file/d/1NIDqQM2nYDMD685JQjRrkfjM9gMxl5P6/view?usp=sharing',
         hrefLabel: 'Abrir manual',
       },
     ]
